551eecacf796fd807e13ad24d792d2cd71f5ee5e
Clinton Magro <clintonmagro@gmail.com>
2018-03-11 12:36:16 +0100

    Added Basic Sample App which just compiles

---
+++ embedded-qpid-sample-app/src/main/java/com/mc/embedded/qpid/sample/EmbeddedQpidSampleApplication.java
@@ -0,0 +1,12 @@
+package com.mc.embedded.qpid.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbeddedQpidSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmbeddedQpidSampleApplication.class, args);
+	}
+}
